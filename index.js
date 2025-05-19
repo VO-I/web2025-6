59fd04ed408df7013413e3f9fe827f17b6fb34e1
Ihor web2025-6 <voichenko.igor06@gmail.com>
2025-05-19 18:26:36 +0300

    2 частина

--- index.js
+++ index.js
@@ -1,34 +1,78 @@
-const { program } = require ('commander');
-const fs = require ('fs');
-const http = require ('http');
-const path = require ('path');
+const { program } = require('commander');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 const express = require("express");
+const multer = require("multer");
+
 const app = express();
 
 program
     .option('-h, --host <address>')
     .option('-p, --port <number>')
-    .option('-c, --cache <path>')
-
+    .option('-c, --cache <path>');
 
 program.parse();
 const options = program.opts();
 
 
-if (!options.cache) {
-    console.log('Please, specify a cache directory');
-    return;
-}
+app.use('/notes/:noteName', express.text());
+app.use(express.urlencoded({ extended: true }));
+app.use('/write', multer().none());
+app.use('/', express.static('public'));
 
-if (!options.port) {
-    console.log('Please, specify port');
-    return;
-}
+app.get('/notes/:noteName', (req, res) => {
+    const notePath = path.join(options.cache, `${req.params.noteName}.txt`);
 
-if (!options.host) {
-    console.log('Please, specify host');
-    return;
-}
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send();
+    }
 
+    const noteContent = fs.readFileSync(notePath);
+    res.send(noteContent);
+});
 
-app.listen(options.port, options.host);
+app.put('/notes/:noteName', (req, res) => {
+    const notePath = path.join(options.cache, `${req.params.noteName}.txt`);
+
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send();
+    }
+
+    fs.writeFileSync(notePath, req.body);
+    res.send();
+});
+
+app.delete('/notes/:noteName', (req, res) => {
+    const notePath = path.join(options.cache, `${req.params.noteName}.txt`);
+
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send();
+    }
+    fs.unlinkSync(notePath);
+    res.send();
+});
+
+app.get('/notes',(req, res) => {
+    const notes = fs.readdirSync(options.cache)
+        .filter((name) => name.endsWith('.txt'))
+        .map((file) => {
+            const noteName = file.replace('.txt', '');
+            const noteText = fs.readFileSync(path.join(options.cache, file), 'utf8');
+            return {name: noteName, text: noteText}
+        });
+    res.status(200).json(notes);
+});
+
+app.post('/write',(req, res) => {
+    const name = req.body.note_name;
+    const note = req.body.note;
+    const filePath = path.join(options.cache, `${name}.txt`);
+    if(fs.existsSync(filePath)){
+        return res.status(400).send();
+    }
+    fs.writeFileSync(filePath, note);
+    res.status(201).send();
+});
+
+app.listen(options.port, options.host); {}
